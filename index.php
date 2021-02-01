e85e93ba29e8501e4f1f52bf9aed0b3123165049
fullstack82 <fullstack8210@gmail.com>
2021-02-01 22:58:18 +0100

    validacion de formulario

--- index.php
+++ index.php
@@ -9,25 +9,35 @@
 
 <body>
     <h1>Validar formularios en php</h1>
+    <?php 
+    if(isset($_GET['error'])){
+        $error = $_GET['error'];
+        if($error == 'faltan_valores'){
+            echo '<strong style="color:red">Introduce todos los datos en todos los campos del formulario</strong>';
+        }
+    }
+    ?>
+
+
     <form method="POST" action="procesar_formulario.php">
 
-    <label for="nombre">Nombre</label><br />
-    <input type="text" name="nombre"><br />
+        <label for="nombre">Nombre</label><br />
+        <input type="text" name="nombre" required="required" pattern="[A-Za-z]+"><br />
 
-    <label for="apellidos">Apellidos</label><br />
-    <input type="text" name="apellidos"><br />
+        <label for="apellidos">Apellidos</label><br />
+        <input type="text" name="apellidos" required="required" pattern="[A-Za-z]+"><br />
 
-    <label for="edad">Edad</label><br />
-    <input type="number" name="edad"><br />
+        <label for="edad">Edad</label><br />
+        <input type="number" name="edad" required="required" pattern="[0-9]+"><br />
 
-    <label for="email">Email</label><br />
-    <input type="email" name="email"><br />
+        <label for="email">Email</label><br />
+        <input type="email" name="email" required="required"><br />
 
-    <label for="pass">Password</label><br />
-    <input type="password" name="pass"><br />
+        <label for="pass">Password</label><br />
+        <input type="password" name="pass" required="required"><br />
 
 
-    <input type="submit" value="Enviar">
+        <input type="submit" value="Enviar">
     </form>
 </body>
 
